97f419d2bd3f055c86d593f1574b9a47eeb1aa35
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-01 20:30:19 +0200

    Modified

---
+++ src/main/java/se/lexicon/Product.java
@@ -0,0 +1,46 @@
+package se.lexicon;
+
+public abstract class Product {
+
+    // Fields
+    private String name;
+    private  double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(double price) {
+
+        this.price = price;
+    }
+
+    public Product() {
+
+    }
+
+    public void setName(String name) {
+        // validate the param if needed
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return "Item: " + name + " , Price: " + price;
+    }
+
+    /*public final String getDescription() {
+        return "Item: " + name + " , Price: " + price;
+    }*/
+
+    public abstract double calculateTax();
+
+}
